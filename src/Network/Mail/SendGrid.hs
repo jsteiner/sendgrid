3bbf43975bbd7c6f8e30f19c90245adbb86e0c03
Josh Steiner <josh@jsteiner.me>
2016-03-29 20:57:05 -0400

    Extracted from https://github.com/thoughtbot/carnival

---
+++ src/Network/Mail/SendGrid.hs
@@ -0,0 +1,30 @@
+module Network.Mail.SendGrid
+    ( sendMailWithSendGrid
+    , sendMailWithSendGridEnv
+    ) where
+
+import Network.Mail.Mime (Mail(..))
+import Network.Mail.SMTP (Password, UserName, sendMailWithLogin')
+import Network.Socket (HostName, PortNumber(..))
+import System.Environment (getEnv)
+
+sendGridHost :: HostName
+sendGridHost = "smtp.sendgrid.net"
+
+sendGridPort :: PortNumber
+sendGridPort = 587
+
+-- | Send a @'Mail'@ with SendGrid using the given credentials
+sendMailWithSendGrid :: UserName -> Password -> Mail -> IO ()
+sendMailWithSendGrid = sendMailWithLogin' sendGridHost sendGridPort
+
+-- | Send a @'Mail'@ with SendGrid using credentials found in the
+--   @SENDGRID_USERNAME@ and @SENDGRID_PASSWORD@ environment variables. Raises
+--   an exception if the environment variables are not set.
+sendMailWithSendGridEnv :: Mail -> IO ()
+sendMailWithSendGridEnv m = do
+    (u, p) <- (,)
+        <$> getEnv "SENDGRID_USERNAME"
+        <*> getEnv "SENDGRID_PASSWORD"
+
+    sendMailWithSendGrid u p m
